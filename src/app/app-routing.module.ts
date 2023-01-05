6ea39e6e42e8900e404e18f3a3cc6648ef8048be
ThatOneGuyRick <rickberg98@live.nl>
2023-01-05 12:51:53 +0100

    moved routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AboutComponent } from './components/about/about.component';
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
-import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,11 +13,6 @@ import { TasksItemComponent } from './components/tasks-item/tasks-item.component
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-const appRoutes: Routes = [
-  {path: '', component: TasksComponent},
-  {path: 'about', component: AboutComponent}
-]
 
 @NgModule({
   declarations: [
@@ -35,7 +30,7 @@ const appRoutes: Routes = [
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes, {enableTracing: true})
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
